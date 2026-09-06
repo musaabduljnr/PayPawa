006581b356f605d779adf31ea0a3e0b5d457c83e
Musa Abubakar <musaabduljnr@gmail.com>
2026-09-06 19:11:47 +0100

    chore: import shared cors in edge functions and ignore supabase temp

--- supabase/functions/squad-gateway/index.ts
+++ supabase/functions/squad-gateway/index.ts
@@ -1,9 +1,5 @@
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
-const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-  'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-};
+import { corsHeaders } from '../_shared/cors.ts';
 
 serve(async (req: Request) => {
   if (req.method === 'OPTIONS') {
